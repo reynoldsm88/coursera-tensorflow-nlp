f35761f50f547fc7529241dc4af0705263e6fb93
Michael Reynolds <michael.reynolds@twosixlabs.com>
2020-02-08 15:26:26 -0500

    actually read the sarcasm dataset and convert into sequences

--- week1/tokenization.py
+++ week1/tokenization.py
@@ -6,35 +6,37 @@ from tensorflow import keras
 import json
 from tensorflow.python.keras.preprocessing.text import Tokenizer
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
+from sys import argv
+from os.path import dirname
 
-data = [ ]
-with open( "sarcasm.json" ) as file:
+with open( f"{dirname( argv[ 0 ] )}/sarcasm.json" ) as file:
     data = json.loads( file.read() )
     for entry in data:
         del entry[ 'article_link' ]
 
-sentences = [ ]
+records = [ ]
 for item in data:
-    sentences.append( (item[ 'headline' ], item[ 'is_sarcastic' ]) )
+    records.append( (item[ 'headline' ], item[ 'is_sarcastic' ]) )
 
-train_set = sentences[ 0:20000 ]
-test_set = sentences[ 19999: -1 ]
+print( records )
+
+train_set = records[ 0:20000 ]
+test_set = records[ 19999: -1 ]
 # setting the number means that the tokenizer will take the top num_words, this is an important hyperparamter
 # that can affect accuracy and training time
 tokenizer = Tokenizer( oov_token = "<OOV>" )
-tokenizer.fit_on_texts( train_set )
-word_index = tokenizer.word_index
 
-test_sentences = [
-    "I love my cat",
-    "I love my dog",
-    "You love my dog!",
-    "Do you think my dog is amazing?",
-]
+training_sentences = [ ]
+training_labels = [ ]
+for record in train_set:
+    sentence, label = record
+    training_sentences.append( sentence )
+    training_labels.append( label )
 
-seq = tokenizer.texts_to_sequences( test_sentences )
+tokenizer.fit_on_texts( training_sentences )
+
+seq = tokenizer.texts_to_sequences( training_sentences )
 # default is to prepend 0's, so padding = post adds 0's to the end, truncating chooses the strategy for where to lost information
 # if a sentence of a different size is encountered in the test_data
 padded = pad_sequences( seq, padding = "post", truncating = "post" )
-print( word_index )
 print( padded )
